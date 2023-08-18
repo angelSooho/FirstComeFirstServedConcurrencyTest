04f3b2fb4b291f07af3939623dca5370fee82e86
SoohoLee <hyeri0603@naver.com>
2023-08-18 21:23:59 +0900

    230818 kafka producer 구현

---
+++ src/main/java/com/example/firstcomefirstservedconcurrencytest/config/KafkaProducerConfig.java
@@ -0,0 +1,40 @@
+package com.example.firstcomefirstservedconcurrencytest.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${kafka.host}")
+    private String kafkaHost;
+
+    @Value("${kafka.port}")
+    private String kafkaPort;
+
+    @Bean
+    public ProducerFactory<String, Long> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost+kafkaPort);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Long> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
