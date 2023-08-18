3942bedca788fbc36f1c8d77fa45bb7d0aef7e84
SoohoLee <hyeri0603@naver.com>
2023-08-18 21:25:09 +0900

    230818 테스트 도움 클래스 구성

---
+++ src/test/java/com/example/firstcomefirstservedconcurrencytest/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package com.example.firstcomefirstservedconcurrencytest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class IntegrationTestSupport {
+}
